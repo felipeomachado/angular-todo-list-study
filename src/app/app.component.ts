900508fcc8779f3c85a8dbf7342a5ee2699c87ce
Felipe Alexandre <felipeomachado@hotmail.com>
2021-03-04 22:19:20 -0300

    add, remove, mark done and mark undone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
+  public todos: Todo[] = [];
+  public title: String = "Minhas Tarefas";
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required,
+      ])]
+    });
+  }
+
+add() {
+  const title = this.form.controls['title'].value;
+  const id = this.todos.length + 1;
+
+  this.todos.push(new Todo(id, title, false));
+
+  this.clear();
+}
+
+clear() {
+  this.form.reset();
+}
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+
+    if(index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+
+  markAsUdone(todo: Todo) {
+    todo.done = false;
+  }
 }
