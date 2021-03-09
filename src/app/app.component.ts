7d4e81331c42a93acc45d582ce3730336e939e5c
Felipe Alexandre <felipeomachado@hotmail.com>
2021-03-08 23:00:43 -0300

    project finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Todo } from 'src/models/todo.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // <app-root>
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   public todos: Todo[] = [];
-  public title: String = "Minhas Tarefas";
   public form: FormGroup;
+  public mode: String = 'list';
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
@@ -20,34 +20,56 @@ export class AppComponent {
         Validators.required,
       ])]
     });
+
+    this.load();
   }
 
-add() {
-  const title = this.form.controls['title'].value;
-  const id = this.todos.length + 1;
+  changeMode(mode: String) {
+    this.mode = mode;
+  }
 
-  this.todos.push(new Todo(id, title, false));
+  add() {
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
+    this.changeMode('list');
+  }
 
-  this.clear();
-}
-
-clear() {
-  this.form.reset();
-}
+  clear() {
+    this.form.reset();
+  }
 
   remove(todo: Todo) {
     const index = this.todos.indexOf(todo);
-
-    if(index !== -1) {
+    if (index !== -1) {
       this.todos.splice(index, 1);
     }
+    this.save();
   }
 
   markAsDone(todo: Todo) {
     todo.done = true;
+    this.save();
   }
 
-  markAsUdone(todo: Todo) {
+  markAsUndone(todo: Todo) {
     todo.done = false;
+    this.save();
+  }
+
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    if (data) {
+      this.todos = JSON.parse(data);
+    } else {
+      this.todos = [];
+    }
   }
 }
